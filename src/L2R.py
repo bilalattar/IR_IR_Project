ef37eb0537c279dd61b6d3357239b994912cf488
Abri <a.r.j.bharos@student.tudelft.nl>
2021-03-03 17:41:04 +0100

    update L2R file

--- src/L2R.py
+++ src/L2R.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 import pyterrier as pt
+from sklearn.ensemble import RandomForestRegressor
 
 if not pt.started():
     pt.init()
-
 
 def filter_topics(t, q):
     data = []
@@ -17,21 +17,31 @@ def filter_topics(t, q):
 
     return newTopics
 
+
 if __name__ == '__main__':
+    dataset = pt.get_dataset("trec-deep-learning-passages")
+    index = pt.IndexFactory.of("./passage_index_correct/data.properties")
+    BM25_br = pt.BatchRetrieve(index, wmodel="BM25", verbose=True) % 1000
+    TF_IDF = pt.BatchRetrieve(index, controls={"wmodel": "TF_IDF"})
+    dev = "dev"
 
-  dataset = pt.get_dataset("trec-deep-learning-passages")
-  index = pt.IndexFactory.of("./passage_index_correct/data.properties")
-  # BM25_br = pt.BatchRetrieve(index, wmodel="BM25", verbose=True) % 1000
+    validation_topics = dataset.get_topics(dev)
+    validation_qrels = dataset.get_qrels(dev)
+    training_topics = dataset.get_topics('train').head(1000)
+    training_qrels = dataset.get_qrels('train')
 
-  dev = "dev"
+    test_topics = dataset.get_topics("test-2019")
+    test_qrels = dataset.get_qrels("test-2019")
 
-  validation_topics = dataset.get_topics(dev)
-  validation_qrels = dataset.get_qrels(dev)
-  training_topics = dataset.get_topics('train').head(10000)
-  training_qrels = dataset.get_qrels('train')
+    filtered_test_topics = filter_topics(test_topics, test_qrels)
+    filtered_training_topics = filter_topics(training_topics, training_qrels).head(1000)
 
-  test_topics = dataset.get_topics("test-2019")
-  test_qrels = dataset.get_qrels("test-2019")
+    pipe = BM25_br >> TF_IDF
+    pipe_fast = pipe.compile()
+    fbr = pt.FeaturesBatchRetrieve(index, controls={"wmodel": "BM25"}, features=["WMODEL:TF_IDF", "WMODEL:PL2"])
 
-  filtered_test_topics = filter_topics(test_topics, test_qrels)
-  filtered_training_topics = filter_topics(training_topics, training_qrels).head(1000)
+    BaselineLTR = pipe_fast >> pt.pipelines.LTR_pipeline(RandomForestRegressor(n_estimators=400))
+    BaselineLTR.fit(filtered_training_topics, training_qrels)
+    results = pt.pipelines.Experiment([BaselineLTR], test_topics, test_qrels, eval_metrics=["ndcg_cut_30"], perquery=True)
+
+    results.to_csv('dfd.csv')
