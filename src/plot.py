b967c52ec225233842e62de762d39404c921bb37
Abri <a.r.j.bharos@student.tudelft.nl>
2021-03-04 19:13:42 +0100

    update files

--- src/plot.py
+++ src/plot.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import pyterrier as pt
 
-if not pt.started():
-    pt.init()
 
 def filter_topics(t, q):
     data = []
@@ -21,19 +18,19 @@ if __name__ == '__main__':
     # name of the csv file to load from
     name = 'plot_files/bm25, tfidf, pl2, dl.csv'
 
-    dataset = pt.get_dataset("trec-deep-learning-passages")
-
-    test_topics = dataset.get_topics("test-2019")
-    test_qrels = dataset.get_qrels("test-2019")
+    test_topics = pd.read_csv('test_topics.csv')
+    test_qrels = pd.read_csv('test_qrels.csv')
 
     filtered_test_topics = filter_topics(test_topics, test_qrels)
 
-    results = pd.read_csv('')
+    results = pd.read_csv(name)
     filtered_test_topics.astype({'qid': 'int64'}).dtypes
     filtered_test_topics["qid"] = pd.to_numeric(filtered_test_topics["qid"])
     results = (filtered_test_topics.set_index('qid').join(results.set_index('qid'))).sort_values(by='value')
     results = results[results['measure'] == 'map']
-    ax = results.plot.barh(x='query', y='value', rot=0, figsize=(15, 20), color='orange')
+    my_colors = [(x / len(filtered_test_topics), x / (2 * len(filtered_test_topics)), 0.75) for x in
+                 range(len(filtered_test_topics))]
+    ax = results.plot.barh(x='query', y='value', rot=0, figsize=(15, 20), color=my_colors)
     plt.ylabel('query')
     plt.xlabel('NDCG@30')
     plt.grid(axis='x')
